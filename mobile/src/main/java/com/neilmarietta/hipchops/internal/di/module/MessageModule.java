4c34f436a43035da4c65637e721f23228d4d1992
Neil Marietta <neil.marietta@ersystems.ch>
2016-04-12 23:10:15 +0200

    Remove unused Scope annotations.

--- mobile/src/main/java/com/neilmarietta/hipchops/internal/di/module/MessageModule.java
+++ mobile/src/main/java/com/neilmarietta/hipchops/internal/di/module/MessageModule.java
@@ -5,7 +5,7 @@ import com.neilmarietta.hipchops.data.repository.EmoticonRepository;
 import com.neilmarietta.hipchops.data.repository.WebPageTitleRepository;
 import com.neilmarietta.hipchops.data.repository.provider.WebPageTitleLocalProvider;
 import com.neilmarietta.hipchops.interactor.MessageUseCase;
-import com.neilmarietta.hipchops.internal.di.PerMessage;
+import com.neilmarietta.hipchops.internal.di.PerActivity;
 import com.neilmarietta.hipchops.util.MessageParser;
 
 import dagger.Module;
@@ -15,25 +15,25 @@ import dagger.Provides;
 public class MessageModule {
 
     @Provides
-    @PerMessage
+    @PerActivity
     MessageUseCase provideMessageUseCase(Gson gson, MessageParser messageParser, EmoticonRepository emoticonRepository) {
         return new MessageUseCase(gson, messageParser, emoticonRepository);
     }
 
     @Provides
-    @PerMessage
+    @PerActivity
     Gson provideGson() {
         return new Gson();
     }
 
     @Provides
-    @PerMessage
+    @PerActivity
     MessageParser provideMessageParser(WebPageTitleRepository webPageTitleRepository) {
         return new MessageParser(webPageTitleRepository);
     }
 
     @Provides
-    @PerMessage
+    @PerActivity
     WebPageTitleLocalProvider provideWebPageTitleLocalProvider() {
         return new WebPageTitleLocalProvider();
     }
